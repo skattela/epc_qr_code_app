71c88432104cebbb8e7c8873966223136e4d3fb8
skattela <2z4H2W4yRe8LkvE>
2020-03-07 08:52:55 +0100

    fix

--- update_webhook_listener/deploy.sh
+++ update_webhook_listener/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker pull skattela/epc_qr_code_app_travis
+docker pull skattela/epc_qr_code_app
 docker stop epc_qr_code_app
 docker rm epc_qr_code_app
-docker run --name epc_qr_code_app -d -p 80:80 skattela/epc_qr_code_app_travis
+docker run --name epc_qr_code_app -d -p 80:80 skattela/epc_qr_code_app
